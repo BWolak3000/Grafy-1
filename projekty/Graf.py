683ab201a72f352db74f988b3516e0e93fd9fce2
Bartoszak <myszak308@wp.pl>
2022-04-11 09:16:19 +0000

    First setup

---
+++ projekty/Graf.py
@@ -0,0 +1,16 @@
+class Graf:
+    def __init__(self):
+        nodes = []
+        list_representation = []
+        matrix_representation = []
+        matrix_incy = []
+
+    class Node:
+        def __init__(self):
+            label = ""
+            edges = []
+
+    class Edge:
+        def __init__(self):
+            label = ""
+            nodes = (None, None)
